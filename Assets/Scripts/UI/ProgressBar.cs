11997a8015a7a1cfd0c2e748fd4b14ef78c6927f
Soroush Hoseinpour <youaremadmadmad@gmail.com>
2019-04-21 13:46:22 +0430

    Load Scene

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour
+{
+    public Image background;
+    public Image progress;
+
+    private void Awake()
+    {
+        StartCoroutine(Progress());
+    }
+
+    private IEnumerator Progress()
+    {
+        while (progress.fillAmount < 1)
+        {
+            progress.fillAmount += Time.deltaTime / 10;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("MenuScene");
+    }
+}
